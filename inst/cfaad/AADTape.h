06ad2c509621c5fc38144f83b691daedf554e7f8
Benjamin Christoffersen <boennecd@gmail.com>
2021-09-09 12:30:16 +0200

    fix issue with if constexpr in #1

--- inst/cfaad/AADTape.h
+++ inst/cfaad/AADTape.h
@@ -82,7 +82,7 @@ public:
 #endif
 
       	//	Store the derivatives and child adjoint pointers unless leaf
-      	if constexpr(N)
+      	if constexpr(N > 0)
       	{
       		node->pDerivatives = myDers.emplace_back_multi<N>();
       		node->pAdjPtrs = myArgPtrs.emplace_back_multi<N>();
